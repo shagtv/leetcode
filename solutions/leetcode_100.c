ecb9b7dab9ef8a34bdedd855a9323c0d2c971857
shagtv <shagtv@eyelinkmedia.com>
2024-02-26 12:52:19 +0000

    LeetCode 100 python, c, swift and kotlin

---
+++ solutions/leetcode_100.c
@@ -0,0 +1,13 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSameTree(struct TreeNode* p, struct TreeNode* q){
+    if (!p || !q) return p == q;
+
+    return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
